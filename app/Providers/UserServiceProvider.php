49ff07f2d5785ab6d25965b99056823cf991c174
Krisna Murti <YggDerasil@gmail.com>
2023-08-05 22:01:50 +0700

    Membuat Login

---
+++ app/Providers/UserServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\Impl\UserServiceImpl;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    public $singletons = [UserService::class => UserServiceImpl::class];
+
+    public function provides()
+    {
+        return [UserService::class];
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
